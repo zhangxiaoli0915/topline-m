3ada8eabac5cac60004b5df2495f2e2563869ff4
zxl <861555291@qq.com>
2020-01-06 15:13:28 +0800

    项目初始化

--- src/main.js
+++ src/main.js
@@ -2,8 +2,17 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import {
+  Button,
+  cell,
+  CellGroup
+
+} from 'vant'
 
 Vue.config.productionTip = false
+Vue.use(Button)
+  .use(cell)
+  .use(CellGroup)
 
 new Vue({
   router,
