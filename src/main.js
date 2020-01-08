148dc4b7a68640eea5162ae8bf171c05306160ae
zxl <861555291@qq.com>
2020-01-09 03:43:35 +0800

    表单验证初始配置

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import './utils/validation.js'
 
 // 加载注册Vant模块
 // import './utils/register-vant'
