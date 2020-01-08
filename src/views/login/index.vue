f89a518a56f71d8758185ffb333af87f8d43be8d
zxl <861555291@qq.com>
2020-01-08 19:02:05 +0800

    登录请求

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -3,32 +3,55 @@
     <van-nav-bar title="登录"/>
     <van-cell-group>
   <van-field
+    v-model="user.mobile"
     required
     clearable
-    label="用户名"
-    placeholder="请输入用户名"
+    label="手机号"
+    placeholder="请输入手机号"
   />
 
   <van-field
-    label="密码"
-    placeholder="请输入密码"
+  v-model="user.code"
+    label="验证码"
+    placeholder="请输入验证码"
     required
   />
 </van-cell-group>
 <div class="login-btn-container">
-  <van-button type="info" >登录</van-button>
+  <van-button type="info" @click="onLogin">登录</van-button>
 </div>
 
   </div>
 </template>
 
 <script>
+import request from '@/utils/request'
 export default {
   name: 'LoginPage',
   component: {},
   props: {},
   data () {
-    return {}
+    return {
+      user: {
+        mobile: '',
+        code: ''
+      }
+    }
+  },
+  methods: {
+    async onLogin () {
+      // 获取表单数据
+      const user = this.user
+      // 表单验证
+      // 请求登录
+      const res = await request({
+        method: 'POST',
+        url: '/app/v1_0/authorizations',
+        data: user
+      })
+      console.log(res)
+    }
+
   }
 
 }
