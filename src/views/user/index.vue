16805aa1cc0d8ebb6c308d8967ca90977ca3afcb
zxl <861555291@qq.com>
2020-01-11 17:51:36 +0800

    用户页面-页面布局

--- src/views/user/index.vue
+++ src/views/user/index.vue
@@ -1,13 +1,97 @@
 <template>
-  <div class="user-container">用户页面</div>
+  <div class="user-container">
+    <van-nav-bar title="黑马头条号" left-arrow />
+    <div class="user-info-container">
+      <div class="row1">
+        <van-image class="col1" fit="cover" round src="https://img.yzcdn.cn/vant/cat.jpeg" />
+        <div class="col2">
+          <div class="row1">
+            <div class="item">
+              <div class="count">123</div>
+              <div class="text">发布</div>
+            </div>
+            <div class="item">
+              <div class="count">123</div>
+              <div class="text">关注</div>
+            </div>
+            <div class="item">
+              <div class="count">123</div>
+              <div class="text">粉丝</div>
+            </div>
+            <div class="item">
+              <div class="count">123</div>
+              <div class="text">获赞</div>
+            </div>
+          </div>
+          <div class="action">
+            <van-button type="primary" size="small">私信</van-button>
+            <van-button type="default" size="small">编辑资料</van-button>
+          </div>
+        </div>
+      </div>
+      <div class="intro-wrap">
+        <div>
+          <span>认证：</span>
+          <span>用户的认证信息</span>
+        </div>
+        <div>
+          <span>简介：</span>
+          <span>用户的简介信息</span>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-export default {
-
-}
+export default {}
 </script>
 
-<style>
-
+<style lang="less" scoped>
+.van-nav-bar .van-icon {
+  color: #fff;
+}
+.user-container {
+  font-size: 14px;
+  .user-info-container {
+    padding: 12px;
+    background-color: #fff;
+    margin-bottom: 10px;
+    .row1 {
+      display: flex;
+      justify-content: space-between;
+      align-items: center;
+      margin-bottom: 10px;
+      .item {
+        text-align: center;
+        .text {
+          font-size: 12px;
+        }
+      }
+      >.col1 {
+        width: 80px;
+        height: 80px;
+      }
+      >.col2 {
+        display: flex;
+        flex-direction: column;
+        justify-content: space-evenly;
+        width: 80%;
+        height: 80px;
+        padding: 0 12px;
+        >.row1 {
+          display: flex;
+          justify-content: space-between;
+        }
+        .action {
+          display: flex;
+          justify-content: space-between;
+          .van-button {
+            width: 45%;
+          }
+        }
+      }
+    }
+  }
+}
 </style>
