e6fbfa1bbb47d5d686a146cb2b2ae31bc296d807
zxl <861555291@qq.com>
2020-01-12 16:21:51 +0800

    首页文章列表-封装文章列表组件

---
+++ src/views/home/components/article-list.vue
@@ -0,0 +1,65 @@
+<template>
+<!-- <div>{{channel.name}}频道的文章列表</div> -->
+   <div class="article-list">
+    <van-list
+      v-model="loading"
+      :finished="finished"
+      finished-text="没有更多了"
+      @load="onLoad"
+    >
+      <van-cell
+        v-for="item in list"
+        :key="item"
+        :title="item"
+      />
+    </van-list>
+  </div>
+</template>
+
+<script>
+// import { getArticlesByChannel } from '@/api/article'
+export default {
+  name: 'ArticleList',
+  components: {},
+  //   props: ['channel'],
+  props: {
+    // 参考文档：https://cn.vuejs.org/v2/guide/components-props.html#Prop-%E9%AA%8C%E8%AF%81
+    channel: {
+      type: Object, // 必须是对象
+      required: true // 必须传递该数据
+    }
+  },
+  data () {
+    return {
+      list: [],
+      loading: false,
+      finished: false
+    }
+  },
+  computed: {},
+  watch: {},
+  created () {},
+  mounted () {},
+  methods: {
+
+    async onLoad () {
+      // 异步更新数据
+      setTimeout(() => {
+        for (let i = 0; i < 10; i++) {
+          this.list.push(this.list.length + 1)
+        }
+        // 加载状态结束
+        this.loading = false
+        // 数据全部加载完成
+        if (this.list.length >= 40) {
+          this.finished = true
+        }
+      }, 500)
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -2,23 +2,30 @@
   <div class="home-container">
       <van-nav-bar title="首页" />
       <van-tabs v-model="active">
-  <van-tab :title="channel.name" v-for="channel in userChannels" :key="channel.id"> 内容 1</van-tab>
+  <van-tab :title="channel.name" v-for="channel in userChannels" :key="channel.id"><article-list :channel="channel"/></van-tab>
   <!-- <van-tab title="标签 2">内容 2</van-tab>
   <van-tab title="标签 3">内容 3</van-tab>
   <van-tab title="标签 4">内容 4</van-tab> -->
   <!-- <h2>{{channel.name}}的文章列表</h2> -->
+
 </van-tabs>
   </div>
 </template>
 
 <script>
 import { getUserChannels } from '@/api/channel'
+import ArticleList from './components/article-list'
 export default {
   // name: 'Homepage',
+  components: {
+    ArticleList
+
+  },
   data () {
     return {
       active: 0,
       userChannels: []
+      // channel: []
     }
   },
   created () {
