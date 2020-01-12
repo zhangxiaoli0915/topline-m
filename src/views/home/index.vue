02b5362100052a67e2828259d396bfc5e9f084cd
zxl <861555291@qq.com>
2020-01-12 09:41:27 +0800

    首页文章列表-展示频道列表

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -2,19 +2,38 @@
   <div class="home-container">
       <van-nav-bar title="首页" />
       <van-tabs v-model="active">
-  <van-tab title="标签 1">内容 1</van-tab>
-  <van-tab title="标签 2">内容 2</van-tab>
+  <van-tab :title="channel.name" v-for="channel in userChannels" :key="channel.id"> 内容 1</van-tab>
+  <!-- <van-tab title="标签 2">内容 2</van-tab>
   <van-tab title="标签 3">内容 3</van-tab>
-  <van-tab title="标签 4">内容 4</van-tab>
+  <van-tab title="标签 4">内容 4</van-tab> -->
+  <!-- <h2>{{channel.name}}的文章列表</h2> -->
 </van-tabs>
   </div>
 </template>
 
 <script>
+import { getUserChannels } from '@/api/channel'
 export default {
+  // name: 'Homepage',
   data () {
     return {
-      active: 2
+      active: 0,
+      userChannels: []
+    }
+  },
+  created () {
+    this.loadUserChannels()
+  },
+  methods: {
+    async loadUserChannels () {
+      try {
+        const { data } = await getUserChannels()
+        this.userChannels = data.data.channels
+        // console.log(data)
+      } catch (err) {
+        console.log(err)
+        this.$toast('获取频道数据失败')
+      }
     }
   }
 
