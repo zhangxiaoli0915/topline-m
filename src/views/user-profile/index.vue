68512099b84039062bc48e7f4d87c2dfa34ca4aa
zxl <861555291@qq.com>
2020-02-04 19:25:45 +0800

    编辑用户资料-页面布局

--- src/views/user-profile/index.vue
+++ src/views/user-profile/index.vue
@@ -1,5 +1,27 @@
 <template>
-  <div class="user-profile">用户资料</div>
+  <div class="user-profile">
+      <div class="user-profile">
+    <van-nav-bar
+      title="编辑资料"
+      left-arrow
+      @click-left="$router.back()"
+    />
+
+    <van-cell-group>
+      <van-cell is-link title="头像">
+        <van-image
+          class="avatar"
+          round
+          src="https://img.yzcdn.cn/vant/cat.jpeg"
+        />
+      </van-cell>
+      <van-cell is-link title="昵称" value="内容" />
+      <van-cell is-link title="介绍" value="内容" />
+      <van-cell is-link title="性别" value="内容" />
+      <van-cell is-link title="生日" value="内容" />
+    </van-cell-group>
+  </div>
+  </div>
 </template>
 
 <script>
@@ -9,6 +31,6 @@ export default {
 }
 </script>
 
-<style>
+<style scoped lang="less">
 
 </style>
