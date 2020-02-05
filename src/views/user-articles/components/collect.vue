a3c1ae08cd9e59fb8428470f8946ecf89936c1c4
zxl <861555291@qq.com>
2020-02-06 01:45:18 +0800

    我的收藏历史-展示数据

--- src/views/user-articles/components/collect.vue
+++ src/views/user-articles/components/collect.vue
@@ -5,13 +5,12 @@
     finished-text="没有更多了"
     @load="onLoad"
   >
-    <!-- <van-cell v-for="item in list" :key="item" :title="item" /> -->
-    <van-cell v-for="(article,index) in list" :key="index" :title="article.title" />
+    <van-cell v-for="(article, index) in list" :key="index" :title="article.title" />
   </van-list>
 </template>
 
 <script>
-import { getCurrentUserArticles } from '@/api/article'
+import { getCollectArticles } from '@/api/article'
 export default {
   name: 'UserArticle',
   components: {},
@@ -22,7 +21,7 @@ export default {
       loading: false,
       finished: false,
       page: 1, // 当前页码
-      perPage: 10// 每页大小
+      perPage: 10 // 每页大小
     }
   },
   computed: {},
@@ -30,32 +29,18 @@ export default {
   created () {},
   mounted () {},
   methods: {
-    // onLoad () {
-    //   // 异步更新数据
-    //   // setTimeout 仅做示例，真实场景中一般为 ajax 请求
-    //   setTimeout(() => {
-    //     for (let i = 0; i < 10; i++) {
-    //       this.list.push(this.list.length + 1)
-    //     }
     async onLoad () {
       // 1. 请求获取数据
-      const { data } = await getCurrentUserArticles({
+      const { data } = await getCollectArticles({
         page: this.page,
         per_page: this.perPage
       })
       // 2. 将数据添加到列表中
       const { results } = data.data
       this.list.push(...results)
-      // // 加载状态结束
-      // this.loading = false
-      // 3.关闭本次loading结束
+      // 3. 关闭本次 loading 结束
       this.loading = false
-      //     // 数据全部加载完成
-      //     if (this.list.length >= 40) {
-      //       this.finished = true
-      //     }
-      //   }, 1000)
-      // 4.判断是否还有数据
+      // 4. 判断是否还有数据
       if (results.length) {
         this.page++ // 更新获取下一页数据的页码
       } else {
--- src/views/user-articles/index.vue
+++ src/views/user-articles/index.vue
@@ -5,8 +5,14 @@
     <!-- /导航栏 -->
 
     <van-tabs v-model="active">
-      <van-tab title="我的收藏">内容 1</van-tab>
-      <van-tab title="我的历史">内容 2</van-tab>
+      <!-- <van-tab title="我的收藏">内容 1</van-tab> -->
+      <van-tab title="我的收藏">
+          <collect-article/>
+      </van-tab>
+      <!-- <van-tab title="我的历史">内容 2</van-tab> -->
+      <van-tab title="我的历史">
+          <history-article/>
+      </van-tab>
       <van-tab title="我的作品">
         <user-article />
       </van-tab>
@@ -15,11 +21,15 @@
 </template>
 
 <script>
+import CollectArticle from './components/collect'
+import HistoryArticle from './components/history'
 import UserArticle from './components/article'
 export default {
   name: 'UserArticles',
   components: {
-    UserArticle
+    UserArticle,
+    CollectArticle,
+    HistoryArticle
   },
   data () {
     return {
