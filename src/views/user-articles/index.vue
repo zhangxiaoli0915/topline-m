3cc76e6bd3b8d8b2029d46a349a2b39e5f4230cf
zxl <861555291@qq.com>
2020-02-06 01:28:57 +0800

    我的收藏历史-布局

--- src/views/user-articles/index.vue
+++ src/views/user-articles/index.vue
@@ -1,10 +1,31 @@
 <template>
-  <div class="user-articles"></div>
+  <div class="user-articles">
+     <!-- 导航栏 -->
+    <van-nav-bar title="我的收藏/历史/作品" left-arrow @click-left="$router.back()" />
+    <!-- /导航栏 -->
+
+    <van-tabs v-model="active">
+      <van-tab title="我的收藏">内容 1</van-tab>
+      <van-tab title="我的历史">内容 2</van-tab>
+      <van-tab title="我的作品">
+        <user-article />
+      </van-tab>
+    </van-tabs>
+  </div>
 </template>
 
 <script>
+import UserArticle from './components/article'
 export default {
-  name: 'UserArticles'
+  name: 'UserArticles',
+  components: {
+    UserArticle
+  },
+  data () {
+    return {
+      active: 0 // 控制激活的标签选项
+    }
+  }
 
 }
 </script>
